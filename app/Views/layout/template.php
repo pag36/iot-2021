b7ac58d23c283faf6bb95aee628e74c86b98c973
od3ng <noprianto@polinema.ac.id>
2021-04-24 10:42:16 +0700

    monitoring sudah ok

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -8,7 +8,6 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
     <title><?= $title; ?></title>
 </head>
 
@@ -17,7 +16,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Polinema</a>
+                <a class="navbar-brand" href="#">Polinema Monitoring System</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -27,13 +26,13 @@
                             <a class="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
                         <li class="nav-item">
+                            <a class="nav-link" href="/pages/sensor">Sensor</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link" href="/pages/about">About</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/pages/contact">Contact</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/pages/sensor">Sensor</a>
                         </li>
                     </ul>
                 </div>
@@ -46,6 +45,7 @@
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
@@ -53,6 +53,8 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
     -->
+
+    <?= $this->renderSection('script'); ?>
 </body>
 
 </html>
--- app/Views/pages/sensor.php
+++ app/Views/pages/sensor.php
@@ -1,138 +1,335 @@
-<!DOCTYPE html>
-<html lang="en">
+<?= $this->extend('layout/template'); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JTI IoT</title>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        var MQTTbroker = 'broker.sinaungoding.com';
-        var MQTTport = 8089;
-        var MQTTsubTopic = 'room/suhu'; //works with wildcard # and + topics dynamically now
-        var chart; // global variuable for chart
-        var dataTopics = new Array();
-        //mqtt broker
-        var client = new Paho.MQTT.Client(MQTTbroker, MQTTport, "jti_" + parseInt(Math.random() * 100, 10));
-        client.onMessageArrived = onMessageArrived;
-        client.onConnectionLost = onConnectionLost;
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div id="container-speed"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div id="chart-suhu"></div><!-- this the placeholder for the chart-->
+        </div>
+        <div class="col">
+            <div id="chart-humadity"></div>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
 
-        //mqtt connecton options including the mqtt broker subscriptions
-        var options = {
-            timeout: 3,
-            useSSL: false,
-            onSuccess: function() {
-                console.log("mqtt connected");
-                // Connection succeeded; subscribe to our topics
-                client.subscribe(MQTTsubTopic, {
-                    qos: 1
-                });
-            },
-            onFailure: function(message) {
-                console.log("Connection failed, ERROR: " + message.errorMessage);
-                //window.setTimeout(location.reload(),20000); //wait 20seconds before trying to connect again.
-            }
-        };
-        //can be used to reconnect on connection lost
-        function onConnectionLost(responseObject) {
-            console.log("connection lost: " + responseObject.errorMessage);
+<!-- template sensor -->
+<?= $this->section('script'); ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+    var MQTTbroker = 'broker.sinaungoding.com';
+    var MQTTport = 8089;
+    var MQTTsubTopic = 'room/#'; //works with wildcard # and + topics dynamically now
+    var chart; // global variuable for chart
+    var chartHum;
+    var chartLamp;
+    var dataTopics = new Array();
+    var dataTopicsHum = new Array();
+    //mqtt broker
+    var client = new Paho.MQTT.Client(MQTTbroker, MQTTport, "jti_" + parseInt(Math.random() * 100, 10));
+    client.onMessageArrived = onMessageArrived;
+    client.onConnectionLost = onConnectionLost;
+
+    //mqtt connecton options including the mqtt broker subscriptions
+    var options = {
+        timeout: 3,
+        useSSL: false,
+        onSuccess: function() {
+            console.log("mqtt connected");
+            // Connection succeeded; subscribe to our topics
+            client.subscribe(MQTTsubTopic, {
+                qos: 1
+            });
+        },
+        onFailure: function(message) {
+            console.log("Connection failed, ERROR: " + message.errorMessage);
             //window.setTimeout(location.reload(),20000); //wait 20seconds before trying to connect again.
-        };
-        //what is done when a message arrives from the broker
-        function onMessageArrived(message) {
-            console.log(message.destinationName, '', message.payloadString);
-            //check if it is a new topic, if not add it to the array
-            if (dataTopics.indexOf(message.destinationName) < 0) {
+        }
+    };
+    //can be used to reconnect on connection lost
+    function onConnectionLost(responseObject) {
+        console.log("connection lost: " + responseObject.errorMessage);
+        //window.setTimeout(location.reload(),20000); //wait 20seconds before trying to connect again.
+    };
+    //what is done when a message arrives from the broker
 
-                dataTopics.push(message.destinationName); //add new topic to array
-                var y = dataTopics.indexOf(message.destinationName); //get the index no
+    function onMessageArrived(message) {
+        // console.log(message.destinationName, '', message.payloadString);
 
-                //create new data series for the chart
-                var newseries = {
-                    id: y,
-                    name: message.destinationName,
-                    data: []
-                };
-                chart.addSeries(newseries); //add the series
-            };
+        //check if it is a new topic, if not add it to the array
+        // if (dataTopics.indexOf(message.destinationName) < 0) {
+        //     dataTopics.push(message.destinationName); //add new topic to array
+        //     var y = dataTopics.indexOf(message.destinationName); //get the index no
+        //     //create new data series for the chart
+        //     var newseries = {
+        //         id: y,
+        //         name: message.destinationName,
+        //         data: []
+        //     };
+        //     console.log(newseries);
+        //     chart.addSeries(newseries); //add the series
 
-            var y = dataTopics.indexOf(message.destinationName); //get the index no of the topic from the array
+        // };
+
+        // var y = dataTopics.indexOf(message.destinationName); //get the index no of the topic from the array
+        // var myEpoch = new Date().getTime(); //get current epoch time
+        // var thenum = message.payloadString.replace(/^\D+/g, ''); //remove any text spaces from the message
+        // var plotMqtt = [myEpoch, Number(thenum)]; //create the array
+        // if (isNumber(thenum)) { //check if it is a real number and not text
+        //     // console.log('is a propper number, will send to chart.')
+        //     plot(plotMqtt, 0, message.destinationName); //send it to the plot function
+        // };
+
+        if (message.destinationName == 'room/lamp') {
+            var point,
+                inc, series;
+            point = chartLamp.series[0].points[0];
+            inc = message.payloadString.replace(/^\D+/g, '');
+            // update series
+            var value = Number(inc);
+            if (value > 0 && value <= 341) {
+                series = 'Gelap';
+            } else if (value > 341 && value <= 682) {
+                series = 'Redup';
+            } else {
+                series = 'Terang';
+            }
+            chartLamp.series[0].name = series;
+            chartLamp.series[0].isDirty = true; // force update tooltips
+            chartLamp.isDirty = true; // force update legend
+            chartLamp.redraw();
+            point.update(value);
+        } else {
             var myEpoch = new Date().getTime(); //get current epoch time
             var thenum = message.payloadString.replace(/^\D+/g, ''); //remove any text spaces from the message
             var plotMqtt = [myEpoch, Number(thenum)]; //create the array
             if (isNumber(thenum)) { //check if it is a real number and not text
-                console.log('is a propper number, will send to chart.')
-                plot(plotMqtt, y); //send it to the plot function
+                // console.log('is a propper number, will send to chart.')
+                plot(plotMqtt, 0, message.destinationName); //send it to the plot function
             };
-        };
-        //check if a real number
-        function isNumber(n) {
-            return !isNaN(parseFloat(n)) && isFinite(n);
-        };
-        //function that is called once the document has loaded
-        function init() {
-            //i find i have to set this to false if i have trouble with timezones.
-            Highcharts.setOptions({
-                global: {
-                    useUTC: false
-                }
-            });
-            // Connect to MQTT broker
-            client.connect(options);
-        };
-        //this adds the plots to the chart
-        function plot(point, chartno) {
-            console.log(point);
+        }
+    };
 
-            var series = chart.series[0],
-                shift = series.data.length > 20; // shift if the series is
-            // longer than 20
-            // add the point
-            chart.series[chartno].addPoint(point, true, shift);
-        };
-        //settings for the chart
-        $(document).ready(function() {
-            chart = new Highcharts.Chart({
-                chart: {
-                    renderTo: 'container',
-                    defaultSeriesType: 'spline'
-                },
-                title: {
-                    text: 'Dashboard IoT JTI - Suhu Live Websockets'
-                },
-                subtitle: {
-                    text: 'broker: ' + MQTTbroker + ' | port: ' + MQTTport + ' | topic : ' + MQTTsubTopic
-                },
-                xAxis: {
-                    type: 'datetime',
-                    tickPixelInterval: 150,
-                    maxZoom: 20 * 1000
-                },
-                yAxis: {
-                    minPadding: 0.2,
-                    maxPadding: 0.2,
-                    title: {
-                        text: 'Value',
-                        margin: 80
-                    }
-                },
-                series: []
-            });
+    //check if a real number	
+    function isNumber(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    };
+
+    //function that is called once the document has loaded
+    function init() {
+
+        //i find i have to set this to false if i have trouble with timezones.
+        Highcharts.setOptions({
+            global: {
+                useUTC: false
+            }
         });
-    </script>
-    <script src="http://code.highcharts.com/stock/highstock.js"></script>
-    <script src="http://code.highcharts.com/stock/modules/exporting.js"></script>
-</head>
-<?= $this->extend('layout/template'); ?>
 
-<?= $this->section('content'); ?>
+        // Connect to MQTT broker
+        client.connect(options);
 
-<body onload="init();">
-    <!--Start the javascript ball rolling and connect to the mqtt broker-->
-    <div id="container" style="height: 500px; min-width: 500px"></div><!-- this the placeholder for the chart-->
-</body>
+    };
+
+
+    //this adds the plots to the chart	
+    function plot(point, chartno, topics) {
+        console.log(point, '', topics);
+
+        if (topics == 'room/suhu') {
+            var series = chart.series[0],
+                shift = series.data.length > 20;
+            chart.series[chartno].addPoint(point, true, shift);
+        } else if (topics == 'room/hum') {
+            var series = chartHum.series[0],
+                shift = series.data.length > 20;
+            chartHum.series[chartno].addPoint(point, true, shift);
+        }
+        // var series = chart.series[0],
+        //     shift = series.data.length > 20; // shift if the series is 
+        // longer than 20
+        // add the point
+        // chart.series[chartno].addPoint(point, true, shift);
+        // chartHum.series[chartno].addPoint(point, true, shift);
+
+    };
+
+    // chart suhu
+    $(document).ready(function() {
+
+        // chart suhu
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'chart-suhu',
+                defaultSeriesType: 'spline'
+            },
+            title: {
+                text: 'Suhu Live Websockets'
+            },
+            subtitle: {
+                text: MQTTbroker + ' | port: ' + MQTTport + ' | topic : ' + 'room/suhu',
+                align: 'center'
+            },
+            exporting: {
+                enabled: false
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150,
+                maxZoom: 20 * 1000
+            },
+            yAxis: {
+                minPadding: 0.2,
+                maxPadding: 0.2,
+                title: {
+                    text: 'Nilai',
+                    margin: 10
+                }
+            },
+            series: [{
+                name: 'room/suhu'
+            }]
+        });
+
+        // chart humadity
+        chartHum = new Highcharts.Chart({
+            chart: {
+                renderTo: 'chart-humadity',
+                defaultSeriesType: 'spline'
+            },
+            title: {
+                text: 'Humadity Live Websockets'
+            },
+            subtitle: {
+                text: MQTTbroker + ' | port: ' + MQTTport + ' | topic : ' + 'room/hum',
+                align: 'center'
+            },
+            exporting: {
+                enabled: false
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150,
+                maxZoom: 20 * 1000
+            },
+            yAxis: {
+                minPadding: 0.2,
+                maxPadding: 0.2,
+                title: {
+                    text: 'Nilai',
+                    margin: 10
+                }
+            },
+            series: [{
+                name: 'room/hum'
+            }]
+        });
+
+        var gaugeOptions = {
+            chart: {
+                type: 'solidgauge'
+            },
+
+            title: {
+                text: 'Cahaya Live Websockets'
+            },
+
+            subtitle: {
+                text: MQTTbroker + ' | port: ' + MQTTport + ' | topic : ' + 'room/lamp',
+                align: 'center'
+            },
+
+            pane: {
+                // center: ['50%', '85%'],
+                // size: '100%',
+                startAngle: -150,
+                endAngle: 150,
+                background: {
+                    backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
+                    innerRadius: '60%',
+                    outerRadius: '100%',
+                    shape: 'arc'
+                }
+            },
+
+            exporting: {
+                enabled: false
+            },
+
+            tooltip: {
+                enabled: true
+            },
+
+            // the value axis
+            yAxis: {
+                stops: [
+                    [0.1, '#55BF3B'], // green
+                    [0.5, '#DDDF0D'], // yellow
+                    [0.9, '#DF5353'] // red
+                ],
+                lineWidth: 0,
+                tickWidth: 0,
+                minorTickInterval: null,
+                tickAmount: 2,
+                title: {
+                    y: -70
+                },
+                labels: {
+                    y: 16
+                }
+            },
+
+            plotOptions: {
+                solidgauge: {
+                    dataLabels: {
+                        y: 5,
+                        borderWidth: 0,
+                        useHTML: true
+                    }
+                }
+            }
+        };
+
+        // The speed gauge
+        chartLamp = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
+            yAxis: {
+                min: 0,
+                max: 1024,
+                // title: {
+                //     text: 'Intensitas Cahaya'
+                // }
+            },
+
+
+            series: [{
+                name: 'lux',
+                data: [0],
+                dataLabels: {
+                    format: '<div style="text-align:center">' +
+                        '<span style="font-size:25px">{y}</span><br/>' +
+                        '<span style="font-size:12px;opacity:0.4">Lux</span>' +
+                        '</div>'
+                },
+                tooltip: {
+                    valueSuffix: ' '
+                }
+            }]
+
+        }));
+
+    });
+</script>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/highcharts-more.js"></script>
+<script src="https://code.highcharts.com/modules/solid-gauge.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/modules/export-data.js"></script>
+<script src="https://code.highcharts.com/modules/accessibility.js"></script>
+<script>
+    init()
+</script>
 <?= $this->endSection(); ?>
-
-</html>
